947dab836bb1576e847c004b6423e4ce94616586
zsimoes <zsimoes@gmail.com>
2018-04-29 01:44:29 +0100

    only hero

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
+import orangeSvg from "./img/orange_shazam.svg";
 //import { Button } from "react-bootstrap";
 
 import "./App.css";
-import Jumbotron from "./components/Jumbotron";
-import Nav from "./components/Nav";
-import Services from "./components/Services";
-
+import Hero from "./components/Hero";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        <Jumbotron />
-        <Services />
-      </div>
+      <Hero>
+          <div className="col">
+            <h1>The perfect <strong><span className="cap">O</span>range</strong></h1>
+            <p className="small">...Is currently rebuilding its website.</p>
+            <p>We believe our digital presence should fully reflect the quality of our work, our organizational culture, and ourselves as people.</p>
+            <img alt="logo" src= {orangeSvg}/>
+            <p>As such, we are carefully planning details, obsessing over fonts, drawing grids, and making orange sketches.</p>
+            <p className="emphasize">Check back on our website soon for a more Perfect Orange</p>
+          </div>
+      </Hero>
     );
   }
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import heroBg from "../img/header-bg.jpg";
+import Particles from 'reactparticles.js';
+import "./hero.css";
+
+class Hero extends React.Component {
+  static propTypes = {
+    backgroundImage: PropTypes.string.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.style = {
+      backgroundImage: "url(" + heroBg + ")"
+    };
+    //merge computed values and custom styles passed in:
+    Object.assign(this.style, props.style);
+  }
+
+  componentDidMount() {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    window.particlesJS.load("particles-js", "particles.json", function() {
+      console.log("callback - particles.js config loaded");
+    });
+  }
+
+  render() {
+    return (
+      <div id="hero" className="container-fluid" style={this.style}>
+        <div className="container-fluid inner">{this.props.children}</div>
+        <Particles id="particles" config="particles.json"/>
+      </div>
+    );
+  }
+}
+
+export default Hero;
